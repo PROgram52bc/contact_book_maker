f77408caac9e930b997807468e91eeb3ace2af4e
wsllet <daviddenghaotian@gmail.com>
2024-06-16 14:10:32 -0400

    parameterize numbers

--- generate.py
+++ generate.py
@@ -2,52 +2,68 @@
 
 from datetime import datetime
 import pandas as pd
-# from PIL import Image
 from fpdf import FlexTemplate,FPDF
 import os
 
-###############
-#  constants  #
-###############
+###########
+#  Paths  #
+###########
 
 image_dir     = "pictures" # the directory for the images
 icon_dir      = "icons"    # the directory for icons
 default_image = os.path.join(icon_dir, "anonymous.jpg") # path to the default image
-
-########################
-#  layout definitions  #
-########################
-
-icons = {
+# path to the icons
+icons         = {
         "email":    os.path.join(icon_dir, "email.png"),
         "children": os.path.join(icon_dir, "children.png"),
         "address":  os.path.join(icon_dir, "address.png"),
         "phone":    os.path.join(icon_dir, "phone.png"),
 }
 
+###############
+#  Constants  #
+###############
+
+num_per_page = 3 # number of items per page
+
+# All units are in inches
+
+page_width  = 3.5
+img_width   = 2
+img_margin  = 0.1 # bottom and top margin for the image
+page_height = 5
+item_height = 1.6
+item_scale  = 1
+
+############
+#  Styles  #
+############
+
+gen_toc          = True # Generate table of content?
+gen_page_num     = True # Generate page number?
+symmetric_layout = True # Symmetric Layout, where the image and description will be reversed on even pages
+reverse_layout   = False # Reverse the order of image and description on all pages
+
+########################
+#  layout definitions  #
+########################
+
 elements = [
-        { 'name': 'english_name',     'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 7, 'x1': 2.2, 'x2': 3.3, 'y1': 0.1,  'y2': 0.2 },
-        { 'name': 'chinese_name',     'type': 'T', 'font': 'kaiti', 'multiline': True, 'align': 'L', 'size': 5, 'x1': 2.2, 'x2': 3.3, 'y1': 0.40, 'y2': 0.45 },
-        { 'name': 'children',         'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 5, 'x1': 2.2, 'x2': 3.3, 'y1': 0.55, 'y2': 0.62 },
-        { 'name': 'children_icon',    'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 2.15, 'x2': 2.2, 'y1': 0.55, 'y2': 0.62 },
-        { 'name': 'children_chinese', 'type': 'T', 'font': 'kaiti', 'multiline': True, 'align': 'L', 'size': 5, 'x1': 2.2, 'x2': 3.3, 'y1': 0.70, 'y2': 0.77 },
-        { 'name': 'address',          'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 4, 'x1': 2.2, 'x2': 3.3, 'y1': 0.85, 'y2': 0.9 },
-        { 'name': 'address_icon',     'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 2.15, 'x2': 2.2, 'y1': 0.85, 'y2': 0.9 },
-        { 'name': 'phone',            'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 4, 'x1': 2.2, 'x2': 3.3, 'y1': 1.0,  'y2': 1.05 },
-        { 'name': 'phone_icon',       'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 2.15, 'x2': 2.2, 'y1': 1.0,  'y2': 1.05 },
-        { 'name': 'email',            'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 4, 'x1': 2.2, 'x2': 3.3, 'y1': 1.15, 'y2': 1.2 },
-        { 'name': 'email_icon',       'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 2.15, 'x2': 2.2, 'y1': 1.15, 'y2': 1.2 },
+        { 'name': 'english_name',     'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 7, 'x1': 0.2,  'x2': 1.3, 'y1': 0.1,  'y2': 0.2 },
+        { 'name': 'chinese_name',     'type': 'T', 'font': 'kaiti', 'multiline': True, 'align': 'L', 'size': 5, 'x1': 0.2,  'x2': 1.3, 'y1': 0.40, 'y2': 0.45 },
+        { 'name': 'children',         'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 5, 'x1': 0.2,  'x2': 1.3, 'y1': 0.55, 'y2': 0.62 },
+        { 'name': 'children_icon',    'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 0.15, 'x2': 0.2, 'y1': 0.55, 'y2': 0.62 },
+        { 'name': 'children_chinese', 'type': 'T', 'font': 'kaiti', 'multiline': True, 'align': 'L', 'size': 5, 'x1': 0.2,  'x2': 1.3, 'y1': 0.70, 'y2': 0.77 },
+        { 'name': 'address',          'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 4, 'x1': 0.2,  'x2': 1.3, 'y1': 0.85, 'y2': 0.9 },
+        { 'name': 'address_icon',     'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 0.15, 'x2': 0.2, 'y1': 0.85, 'y2': 0.9 },
+        { 'name': 'phone',            'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 4, 'x1': 0.2,  'x2': 1.3, 'y1': 1.0,  'y2': 1.05 },
+        { 'name': 'phone_icon',       'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 0.15, 'x2': 0.2, 'y1': 1.0,  'y2': 1.05 },
+        { 'name': 'email',            'type': 'T', 'font': 'hp',    'multiline': True, 'align': 'L', 'size': 4, 'x1': 0.2,  'x2': 1.3, 'y1': 1.15, 'y2': 1.2 },
+        { 'name': 'email_icon',       'type': 'I', 'font': None,    'multiline': None, 'align': 'L', 'size': 4, 'x1': 0.15, 'x2': 0.2, 'y1': 1.15, 'y2': 1.2 },
         ]
 
-# read by default 1st sheet of an excel file
-df = pd.read_excel('info.xlsx')
-fpdf = FPDF(orientation="landscape", format=(1.6, 3.5), unit="in")
-fpdf.set_auto_page_break(False, margin = 0.0)
-fpdf.add_font("kaiti", fname="./simkai.ttf")
-fpdf.add_font("hp", fname="./HPSimplified_Rg.ttf")
-
 #######################
-#  utility functions  #
+#  Utility Functions  #
 #######################
 
 def is_ascii(s):
@@ -78,24 +94,40 @@ def get_img(f, default=default_image, d=image_dir, ext=['png','jpg','jpeg']):
 def nstr(s):
     return str(s) if pd.notnull(s) else ""
 
-for i, row in df.iterrows():
-    img = get_img(nstr(row['key']))
-    print("img: {}".format(img))
-    t1 = FlexTemplate(fpdf, elements=elements)
-    fpdf.add_page(orientation="Landscape", format=(1.6, 3.5))
-    fpdf.image(img, 0.1, 0.1, 0, 1.4)
-    for e in elements:
-        # print("row[e['name']]: {}".format(row[e['name']]))
-        key = e['name']
-        if key in row:
-            print("key: {}".format(key))
-            print("nstr(row[key]): {}".format(nstr(row[key])))
-            t1[key] = nstr(row[key])
-        elif key.endswith("_icon"):
-            pre = key.removesuffix("_icon")
-            if pre in row and pd.notnull(row[pre]):
-                t1[key] = icons[pre]
-    t1.render()
-    # if i >= 5:
-    #     break
-fpdf.output(f"out_{datetime.now().strftime('%Y%m%d%H%M%S')}.pdf")
+##########
+#  Main  #
+##########
+
+def gen_pdf(name):
+# read by default 1st sheet of an excel file
+    df = pd.read_excel(f"{name}.xlsx")
+    fpdf = FPDF(orientation="portrait", format=(page_width, page_height), unit="in")
+    fpdf.set_auto_page_break(False, margin = 0.0)
+    fpdf.add_font("kaiti", fname="./simkai.ttf")
+    fpdf.add_font("hp", fname="./HPSimplified_Rg.ttf")
+
+    print("generating: {}".format(name))
+    for i, row in df.iterrows():
+        print(row['status'])
+        if row['status'] == 'Inactive':
+            print(f"skipping {row['key']} because inactive")
+            continue
+        img = get_img(nstr(row['key']))
+        print("img: {}".format(img))
+        if i % num_per_page == 0:
+            fpdf.add_page(orientation="Portrait", format=(page_width, page_height))
+        t1 = FlexTemplate(fpdf, elements=elements)
+        fpdf.image(img, img_margin, img_margin + item_height * (i % num_per_page), 0, item_scale * (item_height - img_margin * 2))
+        for e in elements:
+            key = e['name']
+            if key in row:
+                t1[key] = nstr(row[key])
+            elif key.endswith("_icon"):
+                pre = key.removesuffix("_icon")
+                if pre in row and pd.notnull(row[pre]):
+                    t1[key] = icons[pre]
+        t1.render(offsetx=img_width, offsety=(i % num_per_page) * item_height, scale=item_scale)
+    fpdf.output(f"{name}_out_{datetime.now().strftime('%Y%m%d%H%M%S')}.pdf")
+
+gen_pdf("info_current")
+# gen_pdf("info_previous")
